8ba3a10ceff51ce1d4d8207c04222e740d67d40b
sumicoding <104206170+sumicoding@users.noreply.github.com>
2022-10-07 17:39:35 +0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var apple,bg,box,coin,ground,spike;
+var appleImg,bgImg,boxImg,coinImg,groundImg,spikeImg;
+
+
+function preload(){
+
+  
+}
+
+
+function setup() {
+  createCanvas(1280,600);
+  createSprite(400, 200, 50, 50);
+}
+
+
+
+function draw() {
+  background("lightGrey");  
+  drawSprites();
+}
